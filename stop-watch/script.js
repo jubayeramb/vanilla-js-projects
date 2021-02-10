63bde9cf9af8b96283ae29168f6128b18719dca9
Jubayer Al Mamun <jubayeramb@gmail.com>
2021-02-11 05:49:51 +0600

    Time laping functionality, progress bar,author added. And some of little changes.

--- stop-watch/script.js
+++ stop-watch/script.js
@@ -1,4 +1,6 @@
 const stopWatch = document.querySelector('#watch')
+const progressBarParentDiv = document.querySelector('.progress');
+const progressBar = document.querySelector('#progress-bar');
 const ulForLaps = document.querySelector("#time-laps");
 
 let hourDisplay = document.querySelector("#hours");
@@ -12,37 +14,41 @@ const lapsBtn = document.querySelector("#laps");
 const resetBtn = document.querySelector("#reset");
 
 // ----hour, minute, second-----  ARE FOR VARIABLES!!! 
-let hour = 0;
-let minute = 0;
+let hr = 0;
+let min = 0;
 let sec = 0;
 let macroSecond = 0;
 let watchStarted = false;
 var timeOutFunc = undefined;
 var timeIntervalFunc = undefined;
 
-
 // ----------defining Time Calculation Function for calculating Sec, Min, Hours, etc. -----------
 timeCalculation = () => {
     if (watchStarted == true) {
+
         sec++;
         if (sec == 60) {
-            minute++;
+            min++;
             sec = 0;
         }
-        if (minute == 60) {
-            hour++;
-            minute = 0;
+        if (min == 60) {
+            hr++;
+            min = 0;
         }
-            if (hour == 24) {
-                hour = 0;
+            if (hr == 24) {
+                hr = 0;
                 clearTimeout(timeOutFunc);
             }
         }
         // displaying time after calculation.
         secondDisplay.textContent = sec < 10 ? "0" + sec : sec;
-        minuteDisplay.textContent = minute < 10 ? "0" + minute : minute;
-        hourDisplay.textContent = hour < 10 ? "0" + hour : hour;
-        
+        minuteDisplay.textContent = min < 10 ? "0" + min : min;
+    hourDisplay.textContent = hr < 10 ? "0" + hr : hr;
+    
+    // progressBar
+    progressBarParentDiv.style.backgroundColor = 'rgb(150, 150, 150)';
+    progressBar.style.width = ((sec/60)*100) + '%';
+
     timeOutFunc = setTimeout(timeCalculation, 1000);
     }
 // defining function for counting Macro Seconds.
@@ -59,39 +65,42 @@ macroSecFunc = () => {
     }
 }
 
-
 //   ----------defining function to hide and show watch after clicking the Stop button-------------
 var watchDisplayed = true;
 var setIntervalForDisplayingWatch = undefined;
 var setTimeOutForDisplayingWatch = undefined;
 
 stopWatchDisplay = () => {
-    setIntervalForDisplayingWatch = setInterval(function () {
-        setTimeOutForDisplayingWatch = setTimeout(function () {
-            if (watchDisplayed == true) {
-                stopWatch.style.display = "none";
-                watchDisplayed = false;
+    if (watchStarted == false) { 
+        setIntervalForDisplayingWatch = setInterval(function () {
+            setTimeOutForDisplayingWatch = setTimeout(function () {
+                if (watchDisplayed == true) {
+                    stopWatch.style.display = "none";
+                    watchDisplayed = false;
+                }
+            }, 500);
+            if (watchDisplayed == false) {
+                stopWatch.style.display = "block";
+                watchDisplayed = true;
             }
-        }, 500);
-        if (watchDisplayed == false) {
-            stopWatch.style.display = "block";
-            watchDisplayed = true;
-        }
-    }, 1000);
+        }, 1000);
+    }
 }
 
 
+
 //    ------------------Event Listener for buttons---------------------------
+
 
 // For Start Button.
 startBtn.addEventListener("click", function (e) {
     if (watchStarted == false) {
         watchStarted = true;
+        startBtn.textContent = "Start";
         macroSecFunc();
         timeCalculation();
-    } else if (watchStarted == true) {
-        stopWatch.style.display = "block";  
-    };
+    }
+    stopWatch.style.display = "block";  
     clearInterval(setIntervalForDisplayingWatch);
     clearTimeout(setTimeOutForDisplayingWatch);
 })
@@ -100,6 +109,7 @@ startBtn.addEventListener("click", function (e) {
 stopBtn.addEventListener("click", function (e) {
     if (watchStarted == true) {
         watchStarted = false;
+        startBtn.textContent = "Resume";
         clearTimeout(timeOutFunc);
         clearInterval(timeIntervalFunc);
         stopWatchDisplay();
@@ -108,7 +118,13 @@ stopBtn.addEventListener("click", function (e) {
 
 // Laps Button.
 lapsBtn.addEventListener("click", function (e) {
-    
+     let hour = hr < 10 ? "0" + hr : hr;
+    let minute = min < 10 ? "0" + min : min;
+    let second = sec < 10 ? "0" + sec : sec;
+
+     let liForLaps = document.createElement("li");
+    liForLaps.appendChild(document.createTextNode(hour + ":" + minute + ":" + second + ":" + macroSecond));
+    ulForLaps.appendChild(liForLaps);
 })
 
 // For Reset Button.
@@ -120,16 +136,20 @@ resetBtn.addEventListener("click", function (e) {
     }
     clearInterval(setIntervalForDisplayingWatch);
     clearTimeout(setTimeOutForDisplayingWatch);
+    progressBarParentDiv.style.backgroundColor = 'rgb(15, 17, 26)';
+    progressBar.style.width = 0;
+    startBtn.textContent = "Start";
     stopWatch.style.display = "block";
         sec = 0;
-        minute = 0;
-        hour = 0;
+        min = 0;
+        hr = 0;
         macroSecond = 0;
         hours.textContent = "00";
         minutes.textContent = "00";
         seconds.textContent = "00";
         macroseconds.textContent = ":00";  //  ====>>>>this one is completely different element with the colone!!!!!!!!
-            
+     ulForLaps.innerHTML = "";
+
 })
 
 
